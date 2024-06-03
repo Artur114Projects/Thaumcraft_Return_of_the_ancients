d1d47eb628011878ce4978eb104817d7fab4dff0
Artur114Projects <artur114projects@gmail.com>
2024-06-03 14:47:12 +0300

    path v1
    Signed-off-by: Artur114Projects <artur114projects@gmail.com>

--- src/main/java/com/artur/returnoftheancients/init/InitDimensions.java
+++ src/main/java/com/artur/returnoftheancients/init/InitDimensions.java
@@ -7,7 +7,7 @@ import net.minecraftforge.common.DimensionManager;
 public class InitDimensions {
     public static final String ancient_world_name = "ancient_world";
     public static final int ancient_world_dim_id = -2147483647;
-    public static final DimensionType ancient_world_dim_type = DimensionType.register(ancient_world_name, "_" + ancient_world_name, ancient_world_dim_id, WorldProviderAncientWorld.class, true);
+    public static final DimensionType ancient_world_dim_type = DimensionType.register(ancient_world_name, "_" + ancient_world_name, ancient_world_dim_id, WorldProviderAncientWorld.class, false);
 
     public static final void registerDimensions()
     {
